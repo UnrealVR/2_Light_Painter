81e53ab1a89a463381b23988a3a3fa29b3b962a2
Sam Pattuzzi <sam@pattuzzi.it>
2018-06-01 18:04:36 +0100

    Add new UI classes

---
+++ UIExperience/Source/UIExperience/SaveGameItem.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SaveGameItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UIEXPERIENCE_API USaveGameItem : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ UIExperience/Source/UIExperience/SaveGameList.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SaveGameList.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UIEXPERIENCE_API USaveGameList : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
