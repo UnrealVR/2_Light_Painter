188ada79c0adfd9d23d3fa7654cfb99add9af30c
Samuel Pattuzzi <sam@pattuzzi.it>
2018-06-12 11:23:42 +0100

    Add buttons and tweak layout using Daydream stickers as a guide.
    Use DMM * 10 in the UI, then reduce scale to 1/100 * distance from viewer.
    
    View sketch file in figma:
    https://www.figma.com/file/cC1VH4xsX8VazeJhLYN90USW/daydream_stickersheet_components_20170517

---
+++ UIExperience/Source/UIExperience/PaintingListSaveGameButtons.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "Components/Button.h"
+
+#include "PaintingListSaveGame.h"
+
+#include "PaintingListSaveGameButtons.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UIEXPERIENCE_API UPaintingListSaveGameButtons : public UUserWidget
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(EditAnywhere)
+	UPaintingListSaveGame* SaveGameList;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* CreateNewButton;
+	
+	UPROPERTY(meta = (BindWidget))
+	UButton* DeleteButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* PrevPageButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* NextPageButton;
+
+};
