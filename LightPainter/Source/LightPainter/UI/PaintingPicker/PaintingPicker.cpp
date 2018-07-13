efadb3548e22bea40a2428bcdbd789ff0684e2be
Samuel Pattuzzi <sam@pattuzzi.it>
2018-07-13 16:17:42 +0100

    1. Add PaintingPicker C++ parent. 2. Create the essential components in C++. 3. Copy the component values. 4. Convert the blueprint to C++.

---
+++ LightPainter/Source/LightPainter/UI/PaintingPicker/PaintingPicker.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PaintingPicker.h"
+
+#include "PaintingGrid.h"
+
+
+// Sets default values
+APaintingPicker::APaintingPicker()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	PaintingGrid = CreateDefaultSubobject<UWidgetComponent>(TEXT("PaintingGrid"));
+	PaintingGrid->SetupAttachment(Root);
+
+	ActionBar = CreateDefaultSubobject<UWidgetComponent>(TEXT("ActionBar"));
+	ActionBar->SetupAttachment(Root);
+}
+
+void APaintingPicker::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UPaintingGrid* Grid = Cast<UPaintingGrid>(PaintingGrid->GetUserWidgetObject());
+	if (Grid)
+	{
+		Grid->AddPainting();
+	}
+}
+
+
