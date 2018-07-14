2b8df61594b30eae83eb67d11c83d38cc70d7927
Samuel Pattuzzi <sam@pattuzzi.it>
2018-07-14 17:37:21 +0100

    1. Add a text box to the card. 2. How the button size can be fixed. 2. Improve spaceing of the grid.

---
+++ LightPainter/Source/LightPainter/UI/PaintingPicker/PaintingGridCard.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PaintingGridCard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIGHTPAINTER_API UPaintingGridCard : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void SetPaintingName(FString PaintingName);
+	
+	
+};
