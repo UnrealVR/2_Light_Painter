b1162719675ab85fb5c20260471601fbe768e7ae
Samuel Pattuzzi <sam@pattuzzi.it>
2018-08-01 10:21:54 +0100

    Create a Pagination Dot Widget
    1. Create the C++ parent.
    2. Create the widget Blueprint and reparent.
    3. Setup with the image widget.
    4. Replace in the grid widget.

---
+++ LightPainter/Source/LightPainter/UI/PaintingPicker/PaginationDot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PaginationDot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIGHTPAINTER_API UPaginationDot : public UUserWidget
+{
+	GENERATED_BODY()	
+	
+};
